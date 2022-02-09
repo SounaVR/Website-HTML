b708045b743dd70aea063e256257cc17dc9082ab
KinchiFR <sounafr@protonmail.com>
2022-02-09 12:02:19 +0100

    tooltip on bs range

--- tp_jeu/js/main.js
+++ tp_jeu/js/main.js
@@ -1,11 +1,23 @@
-var personnages = localStorage.getItem('tabPerso');
-if (personnages) {
-    personnages = JSON.parse(personnages, 'UTF-8');
-} else personnages = [];
+// yeah I know I've put it twice but I need it on the creation page (create.html) 🙃
+const allRanges = document.querySelectorAll(".range-wrap");
+allRanges.forEach(wrap => {
+    const range = wrap.querySelector(".form-range");
+    const bubble = wrap.querySelector(".bubble");
 
-for (let i = 0; i < personnages.length; i++) {
-    const element = personnages[i];
-    var tabPersos = JSON.stringify(personnages);
-    localStorage.setItem('tabPerso', tabPersos);
-    afficheCard(element, mainCard, i);
+    range.addEventListener("input", () => {
+        setBubble(range, bubble);
+    });
+    setBubble(range, bubble);
+});
+
+// See bottom cards.html for more information
+function setBubble(range, bubble) {
+    const val = range.value;
+    const min = range.min ? range.min : 0;
+    const max = range.max ? range.max : 100;
+    const newVal = Number(((val - min) * 100) / (max - min));
+    bubble.innerHTML = val;
+
+    // Sorta magic numbers based on size of the native UI thumb
+    bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
