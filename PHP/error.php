5c7431c3c4f7e11b07ea56f7593776fdd622bbad
KinchiFR <sounafr@protonmail.com>
2022-02-23 01:22:20 +0100

    [PHP] class ErrorHandler

---
+++ PHP/error.php
@@ -0,0 +1,34 @@
+<?php 
+    class ErrorHandler {
+        public string $search = '%s';
+        public string $replace;
+        public string $subject = '
+            <br>
+            <div class="alert alert-danger d-flex align-items-center" role="alert" id="form">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                <div>
+                    %s
+                </div>
+            </div>
+            <br>
+        ';
+
+        function badCredentials() {
+            $replace = 'Your credentials are invalid.';
+            $output = str_replace($this->search, $replace, $this->subject);
+            echo $output;
+        }
+        function emptyFields() {
+            $replace = 'You need to fill every fields.';
+            $output = str_replace($this->search, $replace, $this->subject);
+            echo $output;
+        }
+        function passwordDoesntMatch() {
+            $replace = 'Passwords doesn\'t match.';
+            $output = str_replace($this->search, $replace, $this->subject);
+            echo $output;
+        }
+    }
+    // echo = NULL
+    // return = string(0) ""
+?>
