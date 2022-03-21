bcb3020fd73eed1b74bfbd8695e07785fbc29434
SounaFR <sounafr@protonmail.com>
2022-03-21 15:45:33 +0100

    le site était lourd ça m'a soulé bisous

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -10,31 +10,12 @@ navbar.innerHTML = `
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        JS
-                    </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="/tp_jeu/index.html">TP JEU</a></li>
                         <li><a class="dropdown-item" href="/exercices/javascript/Events-1.html">Events-1</a></li>
                         <li><a class="dropdown-item" href="/exercices/javascript/Events-2.html">Events-2</a></li>
                         <!-- <li><hr class="dropdown-divider"></li> -->
                         <li><a class="dropdown-item" href="/exercices/javascript/Events-3.html">Events-3</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        PHP
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/tp_php/index.php">TP</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        HTML/CSS
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/chez_melo/html/chez_melo.html">TP Chez Mélo</a></li>
                     </ul>
                 </li>
             </ul>
